4f65d0bd310fd0fc26a03688cc1b21d29806f960
SupriyaRajaiahgari <S566492@nwmissouri.edu>
2024-03-04 20:08:27 -0600

    CreateProfile Page Layout

---
+++ app/src/main/java/com/example/fithubapp/CreateProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.fithubapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+    }
+}
