1be73be04786542fe0cc6dfa039574158282611a
Sai Kumar maram <137809859+Saikumr23@users.noreply.github.com>
2024-03-25 11:28:01 -0500

    Update LoginActivity.java
    commit

--- app/src/main/java/com/example/fithubapp/LoginActivity.java
+++ app/src/main/java/com/example/fithubapp/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.fithubapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+<<<<<<< HEAD
+    }
+=======
+    public void OnclickLoginBTN(View view) {
+        Intent intent = new Intent(this, CreateProfileActivity.class);
+        startActivity(intent);
+    }
 }
+>>>>>>> 58f4739bdff354fbcee7e7619f2b9cd0ba1a9f7a
