041d29334e72f950d281d34667461a8d27d3c111
Sai Kumar maram <137809859+Saikumr23@users.noreply.github.com>
2024-04-01 23:00:57 -0500

    modification
    Sai kumar

--- app/src/main/java/com/example/fithubapp/LoginActivity.java
+++ app/src/main/java/com/example/fithubapp/LoginActivity.java
@@ -13,9 +13,23 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-    //Added onClick property
+
     public void OnclickLoginBTN(View view) {
         Intent intent = new Intent(this, CreateProfileActivity.class);
         startActivity(intent);
     }
-}
+
+
+        //Added onClick property
+        public void OnclickRegisterBTN (View view){
+            Intent intent1 = new Intent(this, RegisterActivity.class);
+            startActivity(intent1);
+
+        }
+
+        public void OnclickForgotPasswordBTN (View view){
+            Intent intent2 = new Intent(this, ForgotPasswordActivity.class);
+            startActivity(intent2);
+        }
+    }
+
